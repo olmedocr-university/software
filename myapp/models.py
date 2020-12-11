ff69f7fee3bbadd82c90274c12cf4d5719f4c78e
Pablo Figueroa <pablomf@hotmail.es>
2020-12-11 13:31:41 +0100

    models.py & settings.py changes

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Song (models.Model):
+    name = models.CharField
+    author = models.CharField
+    album = models.CharField
+    duration = models.IntegerField
+
+    def __str__(self):
+        return self.name + " - " + self.author
+
+class User (models.Model):
+    userName = models.CharField
+    email = models.CharField
+
+    def __str__(self):
+        return self.userName + " - " + self.email
+    
+
+class CollaborativeList (models.Model):
+    songs = models.ManyToManyField(Song)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.songs + "\n" + self.users
